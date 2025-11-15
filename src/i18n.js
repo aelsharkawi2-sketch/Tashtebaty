488ee140ca6f90942c833ba092db4f7ab5e7e14d
Mahmoud Soliman <mah123khso@gmail.com>
2025-11-15 02:18:43 +0200

    fixed translation vercel14

--- src/i18n.js
+++ src/i18n.js
@@ -1,24 +1,16 @@
-// src/i18n.js
-import { createI18n } from 'vue-i18n'
+import { createI18n } from "vue-i18n";
 
-// استيراد ملفات JSON مباشرة (Static Import)
-import en from './locals/en.json'
-import ar from './locals/ar.json'
+import ar from "./locals/ar.json";
+import en from "./locals/en.json";
 
-// تحديد اللغة المحفوظة أو استخدام الإنجليزية
-let savedLang = 'en'
-try {
-  const stored = localStorage.getItem('lang')
-  if (stored) savedLang = stored
-} catch (e) {}
-
-export const i18n = createI18n({
+const i18n = createI18n({
   legacy: false,
-  globalInjection: true,
-  locale: savedLang,
-  fallbackLocale: 'en',
+  locale: localStorage.getItem("locale") || "en",
+  fallbackLocale: "en",
   messages: {
-    en:en,
-    ar:ar
-  }
-})
+    en,
+    ar
+  }
+});
+
+export default i18n;
